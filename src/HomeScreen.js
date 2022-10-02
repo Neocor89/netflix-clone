1a7b6c0b0aac66ade0ce1223dc89712d9e0ff5a7
Ben <ncor89b@gmail.com>
2022-10-02 03:10:32 +0200

    Hero Section completed start of API request calls

---
+++ src/HomeScreen.js
@@ -0,0 +1,17 @@
+import Banner from "./Banner";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+
+const HomeScreen = () => {
+  return (
+    <div className="homeScreen">
+      <Nav />
+
+      <Banner />
+
+      {/* : Row */}
+    </div>
+  );
+};
+
+export default HomeScreen;
